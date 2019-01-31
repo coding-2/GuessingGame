758d4f8301dc588d1c1bb37068fa2fd814faacca
conanwho <christianpher2@gmail.com>
2019-01-31 09:45:41 -0800

    added list of words

--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
+from Graphics import Graphics
 
+
+answer = [moonball, history, flag, geyser, squish, quotation, snoot, oblique, ink, dogs, pancake, fox, dragon, turtle, stripes,  ]
 
 
 def __init__():
 
+    #Choose word#
 
+    
 
-
-
+    #The Graphics go here#
