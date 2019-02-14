de1e20c4fcdd4d95a9b05e6c69fb025dac4a2d6c
Shanden <shanmak7@gmail.com>
2019-02-14 09:46:22 -0800

    Update

--- Graphics.py
+++ Graphics.py
@@ -1,10 +1,14 @@
 class Graphics(object):
     guess =  ''
     def startup(self):
-        print ("Welcome to Guesser")
+        print ("Welcome to Guesser!")
 
     def guess(self):
         guess = input("Guess a letter")
         return guess
 
-    def main(self, previous guesses):
+    def main(self, incorrect_guesses, correct_guesses):
+        for i in range(len(correct_guesses)):
+            print(correct_guesses[i], end ="")
+
+        print(len(incorrect_guesses))
