bc466c0b71aebe0484d2f29037dcdc06b435e2b0
Shanden <shanmak7@gmail.com>
2019-02-14 08:40:18 -0800

    Update

--- Graphics.py
+++ Graphics.py
@@ -1,4 +1,10 @@
 class Graphics(object):
+    guess =  ''
     def startup(self):
         print ("Welcome to Guesser")
 
+    def guess(self):
+        guess = input("Guess a letter")
+        return guess
+
+    def main(self):
