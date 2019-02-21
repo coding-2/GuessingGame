da24c95c850b3c354b847241f970ff62092e4e27
Shanden <shanmak7@gmail.com>
2019-02-21 08:40:18 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Graphics import Graphics
 graphics = Graphics()
 
 
-answer = ["zealous", "history", "flag", "geyser", "squish", "quotation", "", "oblique", "ink", "dogs", "pancake", "fox", "dragon", "turtle", "stripes"]
+answer = ["zealous", "history", "moist", "flag", "geyser", "squish", "quotation", "", "oblique", "ink", "dogs", "pancake", "fox", "dragon", "turtle", "stripes"]
 
 
 def function():
